4452ef7e39e8c3c822931a18a79002dfba6c44c1
Fasil <115366522+fasilkppl@users.noreply.github.com>
2024-02-19 21:13:24 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
-import { createClient } from '@supabase/supabase-js'
+dotenv.config(); // Load environment variables from .env file
 
-const supabaseUrl = 'https://lbxaqlbnaznczttahrpi.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxieGFxbGJuYXpuY3p0dGFocnBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY2NDY3MjQsImV4cCI6MTk5MjIyMjcyNH0.3TVPJ76-b93gg_GZoYKKUJnprsdbE7ch0u4YerpGR3A'
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
